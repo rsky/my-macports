9ae9fe9f6a307761221c524dcf4813742532184e
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2012-09-26 12:19:44 +0900

    [py-meld3] Version bumped to 0.6.9.

--- macports/python/py-meld3/files/patch-setup.py
+++ macports/python/py-meld3/files/patch-setup.py
@@ -1,8 +1,8 @@
---- setup.py.orig	2007-10-16 16:08:20.000000000 -0700
-+++ setup.py	2007-10-16 16:09:22.000000000 -0700
+--- setup.py.orig	2012-09-13 01:08:18.000000000 +0900
++++ setup.py	2012-09-26 12:15:07.000000000 +0900
 @@ -1,3 +1,5 @@
 +from ez_setup import use_setuptools
 +use_setuptools()
  from distutils.core import setup, Extension
  import os
- 
+ import sys
