52b16b9bfd5b866ff01ee8cf4ab6394b0091068b
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2013-05-19 19:30:45 +0900

    Correct the port directory name in livecheck.sh

--- livecheck.sh
+++ livecheck.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-find macports -mindepth 2 -maxdepth 2 -type d -print0 | xargs -0 port livecheck
+find ports -mindepth 2 -maxdepth 2 -type d -print0 | xargs -0 port livecheck
