522f3cb162e548c4b58366f7c4c97c43baa8648a
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2014-01-08 10:18:07 +0900

    Remove ports lz4, py-medusa and py-meld3

--- ports/python/py-meld3/files/patch-setup.py
+++
@@ -1,8 +0,0 @@
---- setup.py.orig	2012-09-13 01:08:18.000000000 +0900
-+++ setup.py	2012-09-26 12:15:07.000000000 +0900
-@@ -1,3 +1,5 @@
-+from ez_setup import use_setuptools
-+use_setuptools()
- from distutils.core import setup, Extension
- import os
- import sys
